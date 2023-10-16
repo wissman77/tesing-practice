d6b25776db56f04a68f83d1c818ceb3b98544dbf
Wissam Mansour <wissman77@gmail.com>
2023-10-16 22:55:50 +0300

    add test for already capitalized sentence

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from '../modules/capitalize';
 test('empty string', () => {
   expect(capitalize('')).toBe('');
 });
+
+test('captlized sentence show not changes', () => {
+  expect(capitalize('This day is special.')).toBe('This day is special.');
+});
