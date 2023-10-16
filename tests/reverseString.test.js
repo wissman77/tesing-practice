353cc709d68cc4970ff1ebfd56b065e8108d6d95
Wissam Mansour <wissman77@gmail.com>
2023-10-16 23:21:32 +0300

    add palindrome string test

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -3,3 +3,7 @@ import reverseString from '../modules/reveseString';
 test('empty etring', () => {
   expect(reverseString()).toBe('');
 });
+
+test('palindrome string test', () => {
+  expect(reverseString('racecar')).toBe('racecar');
+});
