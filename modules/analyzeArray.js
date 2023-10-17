980ca9e173b0b5c6a519d52689b0f93e92c00d18
Wissam Mansour <wissman77@gmail.com>
2023-10-17 03:27:16 +0300

    add analyzeArray function impelentation

--- modules/analyzeArray.js
+++ modules/analyzeArray.js
@@ -1,3 +1,16 @@
-function analyzeArray(arr) {}
+function analyzeArray(arr) {
+  if (!arr.length) return {};
+
+  const pickMin = (arr) => Math.min(...arr);
+  const pickMax = (arr) => Math.max(...arr);
+  const getAverage = arr.reduce((prev, curr) => prev + curr, 0) / arr.length;
+
+  return {
+    average: getAverage(),
+    min: pickMin(),
+    max: pickMax(),
+    length: arr.length,
+  };
+}
 
 export default analyzeArray;
