b4a4ddc1e2347ac26b0c783c86f4ece002e5c342
Wissam Mansour <wissman77@gmail.com>
2023-10-17 03:39:45 +0300

    add array with 6 elements test

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,1 +1,14 @@
 import analyzeArray from '../modules/analyzeArray';
+
+test('empty array test', () => {
+  expect(analyzeArray([])).toMatchObject({});
+});
+
+test('array with 6 elements', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
