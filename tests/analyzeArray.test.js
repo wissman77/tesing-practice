b4019ef1d177b05e8a142b7df587d596ae148354
Wissam Mansour <wissman77@gmail.com>
2023-10-17 03:42:23 +0300

    add array with one element test

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -4,6 +4,15 @@ test('empty array test', () => {
   expect(analyzeArray([])).toMatchObject({});
 });
 
+test('array with one element', () => {
+  expect(analyzeArray([4])).toMatchObject({
+    average: 4,
+    min: 4,
+    max: 4,
+    length: 1,
+  });
+});
+
 test('array with 6 elements', () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
     average: 4,
