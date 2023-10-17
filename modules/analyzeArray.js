fda4fcedd2e90eb70feda67d91547cd851afcdd7
Wissam Mansour <wissman77@gmail.com>
2023-10-17 03:38:18 +0300

    simplify the return object

--- modules/analyzeArray.js
+++ modules/analyzeArray.js
@@ -1,14 +1,10 @@
 function analyzeArray(arr) {
   if (!arr.length) return {};
 
-  const pickMin = (arr) => Math.min(...arr);
-  const pickMax = (arr) => Math.max(...arr);
-  const getAverage = arr.reduce((prev, curr) => prev + curr, 0) / arr.length;
-
   return {
-    average: getAverage(),
-    min: pickMin(),
-    max: pickMax(),
+    average: arr.reduce((acc, curr) => acc + curr, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
     length: arr.length,
   };
 }
